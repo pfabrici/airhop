2a346ee711ae53307d2adc8b9d9c4d11ef1f9cca
Peter Fabricius <info@peter-fabricius.de>
2022-01-25 17:38:16 +0100

    rename DAG ping_ip

--- dags/ping_ip.py
+++ dags/ping_ip.py
@@ -18,7 +18,7 @@ def ping_ip():
 
     return pingstatus
 
-dag = DAG('hello_world', description='Hello World DAG',
+dag = DAG('ping_ip', description='Ping network IP',
           schedule_interval='0 12 * * *',
           start_date=datetime(2017, 3, 20), catchup=False)
 
