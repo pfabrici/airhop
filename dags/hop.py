bda3c5f4c395d99ab6386e1274eb9859091ba84f
Peter Fabricius <info@peter-fabricius.de>
2022-01-27 15:58:49 +0100

    fail start of hop

---
+++ dags/hop.py
@@ -0,0 +1,32 @@
+"""
+Code that goes along with the Airflow tutorial located at:
+https://github.com/apache/incubator-airflow/blob/master/airflow/example_dags/tutorial.py
+"""
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from datetime import datetime, timedelta
+
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2015, 6, 1),
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5)
+}
+
+dag = DAG(
+    'tutorial', 
+    default_args=default_args, 
+    schedule_interval=timedelta(days=1))
+
+t1 = BashOperator(
+    task_id='try hop',
+    bash_command='/opt/hop/hop-run.sh',
+    dag=dag)
+
+
+
