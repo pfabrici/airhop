81f6bb13a18e60ef1557c07f69c84db602314c09
Peter Fabricius <info@peter-fabricius.de>
2022-01-27 16:39:20 +0100

    fix typo

--- dags/hop.py
+++ dags/hop.py
@@ -1,7 +1,15 @@
 from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.bash_operator import BashOperator
+from airflow.operators.python_operator import PythonOperator
 from datetime import datetime, timedelta
+
+import os
+
+def run_hop():
+    os.chdir("/opt/hop")
+    os.system("hop-run.sh")
+
 
 with DAG(dag_id='bash_dag', schedule_interval=None, start_date=datetime(2020, 1, 1), catchup=False) as dag:
     # Task 1
@@ -9,6 +17,6 @@ with DAG(dag_id='bash_dag', schedule_interval=None, start_date=datetime(2020, 1,
     # Task 2
     bash_task = BashOperator(task_id='bash_task', bash_command="echo 'command executed from BashOperator'")
     # Task 3
-    hop_task = BashOperator(task_id='hop_task', bash_command="/opt/hop/hop-run.sh")
+    hop_task = PythonOperator(task_id='hop_task', python_callable=run_hop, dag=dag)
     dummy_task >> bash_task
     bash_task >> hop_task
