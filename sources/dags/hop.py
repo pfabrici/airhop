0ae6cb84824cb29a8294d76710b78248e9b27688
Peter Fabricius <info@peter-fabricius.de>
2022-01-28 16:42:15 +0100

    continous logs and return code

--- sources/dags/hop.py
+++ sources/dags/hop.py
@@ -7,21 +7,21 @@ from datetime import datetime, timedelta
 import os
 import subprocess
 
-def run_hop():
+def run_hop_orig():
     os.chdir("/opt/hop")
-    result=subprocess.getoutput("chmod -R 775 /opt/airflow/dags/repo/sources/hop/metadata")
     result=subprocess.getoutput("/opt/hop/hop-run.sh -r local -f generated_rows.hpl")
     print(result)
 
+def run_hop():
+    child = subprocess.Popen("/opt/hop/hop-run.sh -r local -f generated_rows.hpl", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    retValRunJobsSerialScript = 0
+    for line in child.stdout.readlines():
+        child.wait()
+        print line           
+    return(child.returncode)
+
 with DAG(dag_id='bash_dag', schedule_interval=None, start_date=datetime(2020, 1, 1), catchup=False) as dag:
-    # Task 1
-    dummy_task = DummyOperator(task_id='dummy_task')
-    # Task 2
-    bash_task = BashOperator(task_id='bash_task', bash_command="echo 'command executed from BashOperator'")
-    # Task 3
     hop_task = PythonOperator(task_id='hop_task', python_callable=run_hop, dag=dag)
-    # Task 4
     sleep_task = BashOperator(task_id='sleep_task', bash_command="sleep 300")
-    dummy_task >> bash_task
-    bash_task >> hop_task
+
     hop_task >> sleep_task
